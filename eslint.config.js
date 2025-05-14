3dff6f868fd5b58eddd265e48601c2d93805d99a
ctot-nondef <christoph.hoffmann@oeaw.ac.at>
2025-05-14 15:04:47 +0200

    fix: tsconfig

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,11 @@
 import baseConfig from "@acdh-oeaw/eslint-config";
 import vueConfig from "@acdh-oeaw/eslint-config-vue";
 import gitignore from "eslint-config-flat-gitignore";
-
+ 
 const config = [
   gitignore(),
   ...baseConfig,
   ...vueConfig,
-  {
-    rules: {
-    },
-  },
 ];
 
 export default config;
